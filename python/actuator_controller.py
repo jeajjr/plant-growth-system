d956bb4cde459625f55465298256b194ab7ffb58
Jose Almas <jeajjr@gmail.com>
2017-04-02 23:18:50 -0300

    Create actuators controller dummy functions

---
+++ python/actuator_controller.py
@@ -0,0 +1,16 @@
+
+def enableWaterPump():
+    #TODO: enable water pump
+    print('Water pump enabled')
+
+def disableWaterPump():
+    #TODO: disable water pump
+    print('Water pump disabled')
+
+def enableLight():
+    #TODO: enable light
+    print('Light enabled')
+
+def disableLight():
+    #TODO: disable light
+    print('Light disabled')
