8a4db80ae81a22e5936217859ded60f0b7acd105
Jose Almas <jeajjr@gmail.com>
2017-04-03 20:44:07 -0300

    Add data API public and private keys to configurations

--- python/config_ctrl.py
+++ python/config_ctrl.py
@@ -6,20 +6,20 @@ configurationFile = '.config'
 
 # Defines the possible thresholds for turning on the light
 class LightThresholdEnum(Enum):
-    LIGHT_SENSOR = 1
-    TIME = 2
+    LIGHT_SENSOR = 1    # turn on and off according to thresholds
+    TIME = 2            # turn on and off at specific hours every day
 
 # Defines the possible thresholds for turning on the water pump
 class WaterPumpThresholdEnum(Enum):
-    MOISTURE = 1
-    TIME = 2
+    MOISTURE = 1    # turn on and off according to moisture thresholds
+    TIME = 2        # turn on and off in a specific period with a specific duration
 
 class Configurations(object):
     def __init__(self):
         # Light thresholds
-        self.lightThresholdSelect = LightThresholdEnum.LIGHT_SENSOR
-        self.lightSensorUL = 40
-        self.lightSensorLL = 45
+        self.lightThresholdSelect = LightThresholdEnum.TIME
+        self.lightSensorUL = 45
+        self.lightSensorLL = 40
         self.lightTimeON = datetime.time(22, 00)
         self.lightTimeOFF = datetime.time(2, 00)
 
@@ -30,7 +30,12 @@ class Configurations(object):
         self.waterPumpTimePeriod = datetime.time(1, 00)
         self.waterPumpTimeDuration = datetime.time(0, 10)
 
-def storeConfigurations(config: Configurations):
+        self.publicKey = ""
+        self.privateKey = ""
+
+        self.sensorUpdatePeriod = 2 # seconds
+
+def storeConfigurations(config):
     with open(configurationFile, 'wb') as output:
         pickle.dump(config, output, pickle.HIGHEST_PROTOCOL)
 
