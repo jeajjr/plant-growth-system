ccaed8f577eb2ce8bf79c830f8b2cccf5994cc28
Jose Almas <jeajjr@gmail.com>
2017-04-02 23:16:53 -0300

    Create sensor reading dummy functions

---
+++ python/sensor_reader.py
@@ -0,0 +1,26 @@
+
+# TODO: use Raspberry Pi's GPIO
+
+# Returns air humidity from DHT11 sensor in percentage as integer
+def readAirHumidity():
+    # TODO: read actual value from sensor
+    valueRead = 44
+    return valueRead
+
+# Returns air temperature from DHT11 sensor in percentage as float
+def readAirTemperature():
+    # TODO: read actual value from sensor
+    valueRead = 25.5
+    return valueRead
+
+# Returns soil humidity from Flying-Fish sensor in percentage as integer
+def readSoilHumidity():
+    # TODO: read actual value from sensor
+    valueRead = 67
+    return valueRead
+
+# Returns light intensity from GY-302 sensor in percentage as integer
+def readLightIntensity():
+    # TODO: read actual value from sensor
+    valueRead = 85
+    return valueRead
