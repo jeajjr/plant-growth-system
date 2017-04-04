4cebfd34405e4964cb89c089f0ec153d0446848f
Jose Almas <jeajjr@gmail.com>
2017-04-03 21:08:58 -0300

    Add blank Android project

---
+++ android/app/src/main/java/tk/jealmas/plantgrowthcontroller/ActivityMain.java
@@ -0,0 +1,13 @@
+package tk.jealmas.plantgrowthcontroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
