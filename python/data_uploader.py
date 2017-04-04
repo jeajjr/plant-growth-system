194775bb3cef1dc7f95bbf91b0e92e6376606de2
Jose Almas <jeajjr@gmail.com>
2017-04-04 01:17:48 -0300

    Enable data upload

--- python/data_uploader.py
+++ python/data_uploader.py
@@ -4,7 +4,7 @@ from phant import Phant
 
 def postData(pubKey, privKey, air_humidity, air_temp, light, soil_humidity, ctrl_light, ctrl_pump):
     print("sending: ",air_humidity, air_temp, light, soil_humidity, ctrl_light, ctrl_pump)
-#    p = Phant(publicKey=pubKey,fields=["air_humidity","air_temp","light","soil_humidity","ctrl_light","ctrl_pump"],privateKey=privKey)
+    p = Phant(publicKey=pubKey,fields=["air_humidity","air_temp","light","soil_humidity","ctrl_light","ctrl_pump"],privateKey=privKey)
 
-#    p.log(air_humidity, air_temp, light, soil_humidity, ctrl_light, ctrl_pump)
-#    print(p.remaining_bytes, p.cap)
+    p.log(air_humidity, air_temp, light, soil_humidity, ctrl_light, ctrl_pump)
+    print(p.remaining_bytes, p.cap)
